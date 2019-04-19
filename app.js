da570d0e79a644a7a1a7d87a6641cce73099439d
calarence-kong <747097149@qq.com>
2019-04-20 04:03:05 +0800

    天气

---
+++ app.js
@@ -0,0 +1,25 @@
+App({
+  onLaunch() {
+    wx.cloud.init({
+      env: 'envid',
+      traceUser: true,
+    })
+    wx.getSystemInfo({
+      success: (res) => {
+        this.globalData.systeminfo = res
+        this.globalData.isIPhoneX = /iphonex/gi.test(res.model.replace(/\s+/, ''))
+      },
+    })
+  },
+  globalData: {
+    keepscreenon: false,
+    systeminfo: {},
+    isIPhoneX: false,
+    key: '72ce7f633d774f0d8292701424bdebe7',
+    weatherIconUrl: 'https://cdn.heweather.com/cond_icon/',
+    requestUrl: {
+      weather: 'https://free-api.heweather.com/s6/weather',
+      hourly: 'https://free-api.heweather.com/s6/weather/hourly',
+    },
+  },
+})
