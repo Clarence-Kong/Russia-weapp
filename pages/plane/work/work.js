6285199fa0a378f18c2ff88a952fefb59ccfcf6a
iuming <842673556@qq.com>
2019-04-22 19:53:02 +0800

    liuming

--- pages/plane/work/work.js
+++ pages/plane/work/work.js
@@ -1,66 +1,15 @@
-// pages/index/index.js
-Page({
+var pageData = {},
+  type = [
+    'view', 'content', 'form', 'interact', 'nav', 'media', 'map', 'canvas','railway','east'
+  ];
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+pageData.widgetsToggle = function (e) {
+  var id = e.currentTarget.id, data = {};
+  for (var i = 0, len = type.length; i < len; ++i) {
+    data[type[i] + 'Show'] = false;
   }
-})
+  data[id + 'Show'] = !this.data[id + 'Show'];
+  this.setData(data);
+};
+
+Page(pageData);
